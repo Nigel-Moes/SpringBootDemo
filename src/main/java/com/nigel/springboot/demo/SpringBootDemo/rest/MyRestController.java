d23c98950c5983f4238276102e2cb3bee8f75eb8
Nigel <mail@nigelmoes.nl>
2024-03-06 09:50:35 -0500

    Injected custom application properties

--- src/main/java/com/nigel/springboot/demo/SpringBootDemo/rest/MyRestController.java
+++ src/main/java/com/nigel/springboot/demo/SpringBootDemo/rest/MyRestController.java
@@ -1,28 +1,39 @@
 package com.nigel.springboot.demo.SpringBootDemo.rest;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class MyRestController {
 
-    // expose "/" that returns "Hello World!"
+    // inject properties for: coach.name and team.name
+    @Value("${coach.name}")
+    private String coachName;
+    @Value("${team.name}")
+    private String teamName;
 
+    // expose "/" that returns "Hello World!"
     @GetMapping("/")
     public String getHello() {
         return "Hello World!";
     }
 
-    // expose a new endpoint for "workout"
-
+    // expose endpoint for "workout"
     @GetMapping("/workout")
     public String getDailyWorkout() {
         return "Run a 5k!";
     }
 
-    // expose a new endpoint for "fortune"
+    // expose endpoint for "fortune"
     @GetMapping("/fortune")
     public String getDailyFortune() {
         return "Today is your lucky day.";
+    }
+
+    // expose endpoint for "teaminfo"
+    @GetMapping("/teaminfo")
+    public String getTeamInfo() {
+        return "Coach: " + coachName + ", Team: " + teamName;
     }
 }
